a9bebeea1d28b564615a8893fb7fcc44e6322b9e
ElectroFast5 <96546144+ElectroFast5@users.noreply.github.com>
2022-05-17 21:41:47 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,39 @@
-var Electrofast, ElectrofastImg
-var level
+var Electrofast, ElectrofastImg;
+var ground, platformImg, leftWall, rightWall, wallImg, middlePlatform;
 
 function preload() {
-  ElectrofastImg = loadAnimation("ElectroFast1_jump.svg", "ElectroFast2.svg", "ElectroFast3.svg", "ElectroFast4.svg", "ElectroFast5_stand.svg");
+  // This creates ElectroFast's in-game animation. It's pretty long!
+  ElectrofastImg = loadAnimation("Images/Electrofast1_jump.svg", "Images/Electrofast2.svg", "Images/Electrofast3.svg",
+  "Images/Electrofast4.svg", "Images/Electrofast5_stand.svg","Images/Electrofast4.svg","Images/Electrofast3.svg", "Images/Electrofast2.svg");
+
+  // The platforms images:
+  platformImg = loadImage("Images/Ground_Platform.png");
+  wallImg = loadImage("Images/Walls.png");
+
 }
 
 function setup() {
-  createCanvas(1400,700);
-  //createSprite(200, 200, 50, 50);
+  createCanvas(1400, 700);
+
+  // The code below forms ElectroFast's size, picture and animation.
   Electrofast = createSprite(400,400,20,20);
   Electrofast.addAnimation("Electrofast_Running",ElectrofastImg);
-  Electrofast.scale = 0.5
+  Electrofast.scale = 0.32;
+
+  // The platforms ElectroFast stands on:
+  ground = createSprite(700, 670);
+  ground.addImage(platformImg);
+  ground.scale = 1.5;
+
+  leftWall = createSprite(30, 350);
+  leftWall.addImage(wallImg);
+
+  rightWall = createSprite(1370, 350);
+  rightWall.addImage(wallImg);
+
+  middlePlatform = createSprite(700, 430);
+  middlePlatform.addImage(platformImg);
+  middlePlatform.scale =0.5
 }
 
 function draw() {
