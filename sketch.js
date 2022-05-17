02e8c08d399c26e6202a5f845b3b604f8b577928
ElectroFast5 <96546144+ElectroFast5@users.noreply.github.com>
2022-05-17 18:03:34 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,20 @@
+var Electrofast, ElectrofastImg
+var level
+
+function preload() {
+  ElectrofastImg = loadAnimation("ElectroFast1_jump.svg", "ElectroFast2.svg", "ElectroFast3.svg", "ElectroFast4.svg", "ElectroFast5_stand.svg");
+}
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1400,700);
+  //createSprite(200, 200, 50, 50);
+  Electrofast = createSprite(400,400,20,20);
+  Electrofast.addAnimation("Electrofast_Running",ElectrofastImg);
+  Electrofast.scale = 0.5
 }
 
 function draw() {
-  background(255,255,255);  
+  //background(255,255,255);  
+  background("cyan")
   drawSprites();
 }
