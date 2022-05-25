ab24d4bc32de22a5ddb21bebea5238a058f12c7a
Heroic-H <96546194+Heroic-H@users.noreply.github.com>
2022-05-25 17:23:56 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
 var Electrofast, ElectrofastRunImg, ElectrofastStandImg, ElectroFastJumpImg, ElectroFastRunBackwardsImg;
 var ground, platformImg, leftWall, rightWall, wallImg, middlePlatform;
+let lightning, lightningImg, lightningGroup;
+let drones, dronesImg, dronesGroup;
+let bomb, bombImg;
+let gameOver=false;
 
 function preload() {
-  // This creates ElectroFast's in-game animation. It's pretty long!
+  // This creates ElectroFast's in-game animations. It's pretty long!
   ElectrofastRunImg = loadAnimation("Images/Electrofast1_jump.svg", "Images/Electrofast2.svg",
   "Images/Electrofast3.svg", "Images/Electrofast4.svg","Images/Electrofast1_jump.svg","Images/Electrofast2.svg",
   "Images/Electrofast4.svg");
@@ -15,6 +19,10 @@ function preload() {
   platformImg = loadImage("Images/Ground_Platform.png");
   wallImg = loadImage("Images/Walls.png");
 
+  //Image for the gimmicks:
+  dronesImg=loadImage('Images/Drone.png');
+  bombImg=loadImage('Images/Golden Bomb.png');
+  lightningImg=loadImage('Images/Lightning.png');
 }
 
 function setup() {
@@ -42,30 +50,46 @@ function setup() {
   middlePlatform = createSprite(700, 430);
   middlePlatform.addImage(platformImg);
   middlePlatform.scale =0.5;
+
+  //Groups for the gimmicks:
+  dronesGroup=createGroup();
+  lightningGroup=createGroup();
+
+  //THE BOMB😱😱😱:
+  bomb=createSprite(140,550);
+  bomb.addImage(bombImg);
+  bomb.scale=0.5;
 }
 
-function draw() {
-  //background(255,255,255);  
+function draw() { 
   background("cyan");
-  drawSprites();
   controlsAndPhysics();
+  buildDrones();
+
+  if(gameOver==false){
+    drawSprites();
+  }
+
+  if(bomb.isTouching(lightningGroup)||bomb.isTouching(dronesGroup)){
+    gameOver=true;
+  }
 }
 
 function controlsAndPhysics(){
   Electrofast.velocityY+=1;
   Electrofast.collide(ground);
-  Electrofast.collide(middlePlatform)
-  console.log(Electrofast.x)
+  Electrofast.collide(middlePlatform);
+  console.log(Electrofast.x);
 
   // Jumping on the middle platform.
   if(keyDown("UP_ARROW")&&Electrofast.y>380&&Electrofast.y<385&&Electrofast.x>400&&Electrofast.x<1100) {
     Electrofast.velocityY=-20;
- }
+  }
 
  // Jumping on the bottom platform.
   if(keyDown("UP_ARROW")&&Electrofast.y>580&&Electrofast.y<585) {
   Electrofast.velocityY=-20;
-}
+  }
 
   if(keyDown("RIGHT_ARROW")&&!Electrofast.isTouching(rightWall)&&!Electrofast.isTouching(middlePlatform)) {
     Electrofast.x+=10;
@@ -79,5 +103,22 @@ function controlsAndPhysics(){
 
   if(!keyDown("RIGHT_ARROW")&&!keyDown("LEFT_ARROW")) {
     Electrofast.changeImage("stand");
+  }
+}
+
+function buildDrones(){
+  if(frameCount%50==0){
+    drones=createSprite(random(1360,width/2),30,);
+    drones.addImage(dronesImg);
+    drones.scale=0.3;
+    drones.velocityX=-3;
+    drones.velocityY=+3;
+    dronesGroup.collide(leftWall);
+    dronesGroup.collide(rightWall);
+    dronesGroup.collide(ground);
+    dronesGroup.collide(middlePlatform);
+    drones.debug=true;
+    ground.debug=true;
+    dronesGroup.add(drones);
   }
 }
